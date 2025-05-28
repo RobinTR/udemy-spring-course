e54e47a8b16d0187e76bb0ac10aac7c13c840733
Mem Robin Çen <memrobincen@gmail.com>
2025-05-28 11:17:10 +0300

    04-spring-boot-rest-crud-01 Path Variable example

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/robn/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/robn/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.robn.demo.rest;
 import com.robn.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,11 @@ public class StudentRestController {
     public List<Student> getStudents() {
         //Spring Jackson Data Binding is automatically done.
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        //Index of the list. Simple approach for now.
+        return students.get(studentId);
     }
 }
