ee9281aaf337e5fe2b1d4968cd77b4b686d8ceaa
Mem Robin Çen <memrobincen@gmail.com>
2025-05-28 11:09:12 +0300

    04-spring-boot-rest-crud-01 refactor: Student Controller

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/robn/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/robn/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.robn.demo.rest;
 
 import com.robn.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        //Spring Jackson Data Binding is automatically done.
-        List<Student> students = new ArrayList<Student>();
+    private List<Student> students = new ArrayList<Student>();
+
+    @PostConstruct
+    public void loadData() {
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Marry", "Dan"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        //Spring Jackson Data Binding is automatically done.
         return students;
     }
 }
