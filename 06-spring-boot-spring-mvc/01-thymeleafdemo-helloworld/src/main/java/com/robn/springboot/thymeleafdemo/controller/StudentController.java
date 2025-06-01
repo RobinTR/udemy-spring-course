8629093c4b2fcfea443c03a07aead89ea97e3ce9
Mem Robin Çen <memrobincen@gmail.com>
2025-06-01 11:58:02 +0300

    feat: 06-spring-boot-spring-mvc-01 add dynamic country and programming language options to student registration form with Thymeleaf

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/robn/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/robn/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,18 +1,28 @@
 package com.robn.springboot.thymeleafdemo.controller;
 
 import com.robn.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
+    @Value("${programming-languages}")
+    private List<String> programmingLanguages;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
+        model.addAttribute("programmingLanguages", programmingLanguages);
 
         return "student-form";
     }
