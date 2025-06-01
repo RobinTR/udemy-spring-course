d38f81e08d7af6680bef52121c3c7fc1ca5ace03
Mem Robin Çen <memrobincen@gmail.com>
2025-06-01 18:34:40 +0300

    feat(06-spring-boot-spring-mvc-02): add Spring Validation and InitBinder for input trimming

---
+++ 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/robn/springdemo/mvc/CustomerController.java
@@ -0,0 +1,38 @@
+package com.robn.springdemo.mvc;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        //pre-process all String fields: trim leading and trailing white space. if string only has white space trim it to null
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @GetMapping("/")
+    public String showForm(Model model) {
+        model.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "customer-form";
+        }
+
+        return "customer-confirmation";
+    }
+}
