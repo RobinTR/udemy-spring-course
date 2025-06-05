e0ddaccfd496299eb4fb770f441e87896a02b5e5
Mem Robin Çen <memrobincen@gmail.com>
2025-06-05 15:16:09 +0300

    feat(09-spring-boot-jpa-advanced-mappings-04): implement uni-directional mapping and related methods

---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/robn/cruddemo/dao/AppDAO.java
@@ -0,0 +1,23 @@
+package com.robn.cruddemo.dao;
+
+import com.robn.cruddemo.entity.Course;
+import com.robn.cruddemo.entity.Instructor;
+import com.robn.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+    Instructor findInstructorById(Integer id);
+    void deleteInstructorById(Integer id);
+    InstructorDetail findInstructorDetailById(Integer id);
+    void deleteInstructorDetailById(Integer id);
+    List<Course> findCoursesByInstructorId(Integer instructorId);
+    Instructor findInstructorByIdJoinFetch(Integer id);
+    void update(Instructor instructor);
+    void update(Course course);
+    Course findCourseById(Integer id);
+    void deleteCourseById(Integer id);
+    void save(Course course);
+    Course findCourseAndReviewsByCourseId(Integer id);
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/robn/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,124 @@
+package com.robn.cruddemo.dao;
+
+import com.robn.cruddemo.entity.Course;
+import com.robn.cruddemo.entity.Instructor;
+import com.robn.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+    private final EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(Integer id) {
+        return entityManager.find(Instructor.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(Integer id) {
+        Instructor instructor = entityManager.find(Instructor.class, id);
+        List<Course> courses = instructor.getCourses();
+
+        for (Course course : courses) {
+            // Break the association with the instructor
+            course.setInstructor(null);
+        }
+
+        entityManager.remove(instructor);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(Integer id) {
+        return entityManager.find(InstructorDetail.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(Integer id) {
+        InstructorDetail instructorDetail = entityManager.find(InstructorDetail.class, id);
+        //Remove the associciated object reference
+        //to break the bidirectional relationship
+        instructorDetail.getInstructor().setInstructorDetail(null);
+        entityManager.remove(instructorDetail);
+    }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(Integer instructorId) {
+        TypedQuery<Course> query = entityManager.createQuery("FROM Course WHERE instructor.id = :instructorId", Course.class);
+        query.setParameter("instructorId", instructorId);
+        List<Course> courses = query.getResultList();
+
+        return courses;
+    }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(Integer id) {
+        TypedQuery<Instructor> query = entityManager.createQuery("SELECT i FROM Instructor i " +
+                "JOIN FETCH i.courses " +
+                "JOIN FETCH i.instructorDetail " +
+                "WHERE i.id = :id", Instructor.class);
+        query.setParameter("id", id);
+        Instructor instructor = query.getSingleResult();
+
+        return instructor;
+    }
+
+    @Override
+    @Transactional
+    public void update(Instructor instructor) {
+        entityManager.merge(instructor);
+    }
+
+    @Override
+    @Transactional
+    public void update(Course course) {
+        entityManager.merge(course);
+    }
+
+    @Override
+    public Course findCourseById(Integer id) {
+        return entityManager.find(Course.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCourseById(Integer id) {
+        Course course = entityManager.find(Course.class, id);
+        entityManager.remove(course);
+    }
+
+    @Override
+    @Transactional
+    public void save(Course course) {
+        entityManager.persist(course);
+    }
+
+    @Override
+    public Course findCourseAndReviewsByCourseId(Integer id) {
+        TypedQuery<Course> query = entityManager.createQuery("SELECT c FROM Course c " +
+                "JOIN FETCH c.reviews " +
+                "WHERE c.id = :courseId", Course.class);
+        query.setParameter("courseId", id);
+        Course course = query.getSingleResult();
+
+        return course;
+    }
+}
