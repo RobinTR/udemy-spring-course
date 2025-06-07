9ea2d2e4d3573a702d5c7d2845130b5581135384
Mem Robin Çen <memrobincen@gmail.com>
2025-06-07 11:08:39 +0300

    feat(10-spring-boot-aop-01): demonstrate basic usage of AOP @Before advice

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/robn/aopdemo/AopdemoApplication.java
@@ -0,0 +1,29 @@
+package com.robn.aopdemo;
+
+import com.robn.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(AccountDAO accountDAO) {
+		return runner -> {
+			demoTheBeforeAdvice(accountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO accountDAO) {
+		accountDAO.addAccount();
+		System.out.println("\n let's call the addAccount method again");
+		accountDAO.addAccount();
+	}
+
+}
---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/robn/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,5 @@
+package com.robn.aopdemo.dao;
+
+public interface AccountDAO {
+    void addAccount();
+}
