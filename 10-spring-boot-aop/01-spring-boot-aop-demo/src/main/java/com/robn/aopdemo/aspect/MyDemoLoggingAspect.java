fb998773c8660a92db18aa7a0b0b4418c72b9708
Mem Robin Çen <memrobincen@gmail.com>
2025-06-07 11:27:28 +0300

    feat(10-spring-boot-aop-01): AOP: Pointcut Expressions examples

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/robn/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/robn/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -10,8 +10,22 @@ public class MyDemoLoggingAspect {
     // This is where we add all of our related advices for logging
 
     // Let's start with @Before advice
+    /*
     @Before("execution(public void addAccount())")
     public void beforeAddAccountAdvice() {
         System.out.println("\n======>>> Executing @Before advice on addAccount()");
+    }
+     */
+    /*
+    @Before("execution(public void com.robn.aopdemo.dao.AccountDAO.addAccount())")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n======>>> Executing @Before advice on addAccount()");
+    }
+     */
+    //@Before("execution(public void add*())")
+    //@Before("execution(void add*())")
+    @Before("execution(* add*())")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n======>>> Executing @Before advice on add*()");
     }
 }
---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/robn/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,5 @@
+package com.robn.aopdemo.dao;
+
+public interface MembershipDAO {
+    boolean addSillyMember();
+}
